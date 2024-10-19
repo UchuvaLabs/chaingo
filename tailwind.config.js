8c639b42d7e95ff8f50389d744df12c9c1fb151f
Samuel Sml <veramirandasamuel6@gmail.com>
2024-10-19 14:22:46 -0500

    chore: add colors used in tailwind cofiguration

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,17 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        background: "#121212", // Fondo principal
+        primaryText: "#F5F5F5", // Texto principal
+        secondaryText: "#A0A0A0", // Texto secundario
+        accent: "#ffb300", // Color de acento (botones, enlaces)
+        success: "#28A745", // Colores de éxito
+        warning: "#FFC107", // Colores de advertencia
+        error: "#DC3545", // Colores de error
+        border: "#2C2C2C", // Color de bordes
+        cardBackground: "#1F1F1F", // Fondo de tarjetas o contenedores
+      },
     },
     fontFamily: {
       sans: ["Roboto", "sans-serif"],
