7515fa6bc4e59ff0066353b3f2d47223a38a9d3f
Samuel Sml <veramirandasamuel6@gmail.com>
2024-10-19 15:08:40 -0500

    chore: updated route to click in img chaingo

--- src/components/landing/Navbar.tsx
+++ src/components/landing/Navbar.tsx
@@ -2,7 +2,7 @@ const Navbar = () => {
   return (
     <nav className="border-border bg-background sticky top-0 z-50">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
+        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img
             src="/src/assets/img/chaingov1.png"
             className="h-14 w-auto"
