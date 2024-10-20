73b0eb6793ae1bf80048e06b06a82737c9d081bd
Samuel Sml <veramirandasamuel6@gmail.com>
2024-10-19 19:36:12 -0500

    feat: config to framer motion

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
+  server: {
     host: true,
-    port: 5173
-  }
-})
+    port: 5173,
+  },
+  optimizeDeps: {
+    exclude: ["framer-motion"],
+  },
+});
